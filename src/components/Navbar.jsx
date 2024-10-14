329f17c35795e33f0d35b70756231b350a1befad
projectBazaar <sarcasticgamer96@gmail.com>
2024-10-14 23:45:39 +0530

    changed navbar.jsx truncate()

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -137,7 +137,7 @@ export default function Navbar() {
               )}
               <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none">
                 <span className="text-blue-700 font-mono p-2 m-auto">
-                  {currentUser && truncate(currentUser.displayName)}
+                  {currentUser && truncate(currentUser?.displayName)}
                 </span>
 
                 <MenuItem>
